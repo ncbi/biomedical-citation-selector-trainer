957ad4ff4c2b51ce54b62be79bba48f4f0187ea1
raear <raear@nih.gov>
2022-11-23 14:05:53 -0500

    Add biowulf logging

--- train_on_biowulf.sh
+++ train_on_biowulf.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-sbatch --partition=gpu --cpus-per-task=8 --gres=gpu:p100:1 --mem=64g --time=10-00:00:00 train.sh
+sbatch --output="/data/raear/logs/%j.out" --error="/data/raear/logs/%j.out" --partition=gpu --cpus-per-task=8 --gres=gpu:p100:1 --mem=64g --time=10-00:00:00 train.sh
